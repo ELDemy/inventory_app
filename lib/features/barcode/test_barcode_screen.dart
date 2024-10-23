43c99b4a1f7ccbe9414d6f62d4187ec02b011ad2
MELDemy <mahmoudeldemerdash5@gmail.com>
2024-10-23 22:36:46 +0300

    setting up firebase enviroment

--- lib/features/barcode/test_barcode_screen.dart
+++ lib/features/barcode/test_barcode_screen.dart
@@ -24,7 +24,6 @@ class _TestBarcodeState extends State<TestBarcode> {
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> _scanBarcodeNormal() async {
     String barcodeScanRes = await BarcodeUtil.scanBarcodeNormal();
-
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
